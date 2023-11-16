ecde5e0464e9fcb088a104a408be7f52375bb071
lcontr1 <contrerasliliana22@gmail.com>
2023-11-16 14:35:05 -0600

    removed unnecessary code

--- froyo.js
+++ froyo.js
@@ -7,13 +7,6 @@ const userInputString = prompt(
 // Split the string of numbers into an array of strings.
 const stringArray = userInputString.split(",");
 
-// Convert the array of strings into an array of numbers.
-/*const numbers = [];
-for (let i = 0; i < stringArray.length; i++) {
-  const str = stringArray[i];
-  const number = parseInt(str);
-  numbers.push(number);
-}*/
 
 // count the amount of flavors typed in
 const counts = {}
@@ -26,18 +19,3 @@ console.table(counts)
 
 
 
-/*const vanilla = []
-const strawberry = []
-const coffee = []
-while (stringArray) {
-  if (stringArray === "vanilla") {
-    vanilla++
-  }
-  else if (stringArray === "strawberry") {
-    strawberry++
-  }
-  else if (stringArray === "coffee") {
-    coffee++
-  }
-
-}*/
